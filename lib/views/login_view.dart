46ea762005e329a7c07f9ce4ebb840efb28c5b06
Davis-Xtranet <davis.wafula@xtranet.co.ke>
2021-05-24 14:32:55 +0300

    work

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -7,18 +7,25 @@ import 'package:uzachap/views/otp_view.dart';
 import 'package:uzachap/widgets/custom_btn.dart';
 import 'package:uzachap/widgets/custom_input.dart';
 
+import 'towns_view.dart';
+
+enum MobileVerificationState {
+  SHOW_MOBILE_FORM_STATE,
+  SHOW_OTP_FORM_STATE,
+}
 class LoginView extends StatefulWidget {
   @override
   _LoginViewState createState() => _LoginViewState();
 }
 
 class _LoginViewState extends State<LoginView> {
-
+  MobileVerificationState currentState =
+      MobileVerificationState.SHOW_MOBILE_FORM_STATE;
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseAuth _auth  =FirebaseAuth.instance;
 
-  SmsAutoFill smsAutoFill = SmsAutoFill();
+
 
   // Default Form Loading State
   bool _loginFormLoading = false;
@@ -27,8 +34,37 @@ class _LoginViewState extends State<LoginView> {
 
   // Form Input Field Values
   String _phoneNumEditingController = "";
+  String otpController = "";
   // Focus Node for input fields
   FocusNode _phoneFocusNode;
+
+  void signInWithPhoneAuthCredential(
+      PhoneAuthCredential phoneAuthCredential) async {
+    setState(() {
+      _loginFormLoading = true;
+    });
+
+    try {
+      final authCredential =
+      await _auth.signInWithCredential(phoneAuthCredential);
+
+      setState(() {
+        _loginFormLoading = false;
+      });
+
+      if(authCredential?.user != null){
+        Navigator.push(context, MaterialPageRoute(builder: (context)=> TownsView()));
+      }
+
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _loginFormLoading = false;
+      });
+
+      _scaffoldKey.currentState
+          .showSnackBar(SnackBar(content: Text(e.message)));
+    }
+  }
   @override
   void initState() {
     _phoneFocusNode = FocusNode();
@@ -41,74 +77,9 @@ class _LoginViewState extends State<LoginView> {
     super.dispose();
   }
 
-  void verifyPhoneNumber() async{
-    print("*************************************+254$_phoneNumEditingController");
-
-    //Callback for when the user has already previously signed in with this phone number on this device
-    PhoneVerificationCompleted verificationCompleted =
-        (PhoneAuthCredential phoneAuthCredential) async {
-
-      await auth.signInWithCredential(phoneAuthCredential);
-      showSnackbar("Phone number automatically verified");
-    };
-
-    //Listens for errors with verification, such as too many attempts
-    PhoneVerificationFailed verificationFailed =
-        (FirebaseAuthException authException) {
-      showSnackbar('Phone number verification failed.');
-    };
-
-    //Callback for when the code is sent
-    PhoneCodeSent codeSent =
-        (String verificationId, [int forceResendingToken]) async {
-      showSnackbar('Please check your phone for the verification code.');
-      _verificationId = verificationId;
-    };
-
-    PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
-        (String verificationId) {
-      showSnackbar("verification code: " + verificationId);
-      _verificationId = verificationId;
-    };
-
-    try {
-      await auth.verifyPhoneNumber(
-          phoneNumber: "+254"+_phoneNumEditingController,
-          timeout: const Duration(seconds: 5),
-          verificationCompleted: verificationCompleted,
-          verificationFailed: verificationFailed,
-          codeSent: codeSent,
-          codeAutoRetrievalTimeout: codeAutoRetrievalTimeout).then((value){
-        Navigator.push(
-            context,
-            PageTransition(
-                child: OTPView(
-                  verId: _verificationId,
-                  uid: FirebaseAuth.instance
-                      .currentUser.uid,
-                ),
-                type: PageTransitionType.rightToLeft));
-      });
-    } catch (e) {
-      showSnackbar("Failed to Verify Phone Number");
-    }
-  }
-
-  void showSnackbar(String message) {
-    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(message)));
-  }
-
-void signInWithPhoneNumber(){
-
-}
-
-  @override
-  Widget build(BuildContext context) {
-
-
+  getMobileFormWidget(context){
     return SafeArea(
       child: Scaffold(
-        key: _scaffoldKey,
         body: Container(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
@@ -136,43 +107,43 @@ void signInWithPhoneNumber(){
                   ),
                   Row(
 
-                   children: [
-                     Container(
-                       width: 95.0,
-                       margin: EdgeInsets.symmetric(
-                         vertical: 8.0,
-                         horizontal: 24.0,
-                       ),
-                       decoration: BoxDecoration(
-                           border: Border.all(color: Colors.grey.shade400, width: 1.6),
-                           color: Colors.transparent,
-                           borderRadius: BorderRadius.circular(8.0)
-                       ),
-                       child: TextField(
-                         decoration: InputDecoration(
-                             border: InputBorder.none,
-                             hintText: "+254",
-                             contentPadding: EdgeInsets.symmetric(
-                               horizontal: 24.0,
-                               vertical: 18.0,
-                             )
-                         ),
-                       ),
-                     ),
-                     Flexible(
-                       child: CustomInput(
-                         onChanged: (value) {
-                           _phoneNumEditingController = value;
-                         },
-                         onSubmitted: (value) {
-                           _phoneFocusNode.requestFocus();
-                         },
-                         hintText: '70 000 000 00',
-                         textInputAction: TextInputAction.done,
-                       ),
-                     ),
+                    children: [
+                      Container(
+                        width: 95.0,
+                        margin: EdgeInsets.symmetric(
+                          vertical: 8.0,
+                          horizontal: 24.0,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey.shade400, width: 1.6),
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(8.0)
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "+254",
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 24.0,
+                                vertical: 18.0,
+                              )
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        child: CustomInput(
+                          onChanged: (value) {
+                            _phoneNumEditingController = value;
+                          },
+                          onSubmitted: (value) {
+                            _phoneFocusNode.requestFocus();
+                          },
+                          hintText: '70 000 000 00',
+                          textInputAction: TextInputAction.done,
+                        ),
+                      ),
 
-                   ],
+                    ],
                   ),
                   SizedBox(
                     height: 40,
@@ -180,8 +151,33 @@ void signInWithPhoneNumber(){
                   CustomBtn(
                     text: "Get OTP",
                     onPressed: () async {
-                      verifyPhoneNumber();
+                      setState(() {
+                        _loginFormLoading = true;
+                      });
+                      await _auth.verifyPhoneNumber(
+                          phoneNumber: "+254"+_phoneNumEditingController,
+                          verificationCompleted: (phoneAuthCredential) async{
+                            setState(() {
+                              _loginFormLoading = false;
+                            });
+                          },
+                          verificationFailed: (verificationFailed) async{
+                            setState(() {
+                              _loginFormLoading = false;
+                            });
+                            _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(verificationFailed.message),));
+                          },
+                          codeSent: (_verificationId, resendingToken) async{
+                            setState(() {
+                              _loginFormLoading = false;
+                              currentState = MobileVerificationState.SHOW_OTP_FORM_STATE;
+                              this._verificationId = _verificationId;
+                            });
+                          },
+                        codeAutoRetrievalTimeout: (verificationId) async{
 
+                        }
+                      );
                     },
                     isLoading: _loginFormLoading,
                   ),
@@ -202,5 +198,75 @@ void signInWithPhoneNumber(){
         ),
       ),
     );
+  }
+
+  getOtpFormWidget(context){
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/back.png"), fit: BoxFit.cover),
+          ),
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  Image.asset("assets/car.png", width: MediaQuery.of(context).size.width,),
+                  SizedBox(height: 90,),
+                  Column(children: [
+                    Text("Enter the code here", style: TextStyle(color: darkText.withOpacity(0.5), fontSize: 18.0, wordSpacing: 3.0, fontWeight: FontWeight.w500),),
+                    SizedBox(height: 10.0),
+                    Align(
+                        alignment: Alignment.center,
+                        child: Image.asset("assets/arrow.png", height: 12,)),
+                  ],
+
+                  ),
+                  SizedBox(height: 10.0),
+                  CustomInput(
+                    hintText: '70 000 000 00',
+                    textInputAction: TextInputAction.done,
+                  ),
+                  SizedBox(height: 10.0),
+                  Text("Only correct code will log you in!", style: TextStyle(color: darkText.withOpacity(0.5), fontSize: 16.0, wordSpacing: 2.0, fontWeight: FontWeight.w500),),
+                  SizedBox(height: 20.0),
+                  CustomBtn(
+                    text: "Verify",
+                    onPressed: () async {
+                      PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.credential(verificationId: _verificationId, smsCode: otpController);
+                      signInWithPhoneAuthCredential(phoneAuthCredential);
+                    },
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text("Uzachap", style: TextStyle(color: darkText.withOpacity(0.5), fontSize: 12.0, wordSpacing: 0.5, fontWeight: FontWeight.w300),)),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Container(
+        child: _loginFormLoading ? Center(
+          child: CircularProgressIndicator(),
+        ) : currentState == MobileVerificationState.SHOW_MOBILE_FORM_STATE
+        ? getMobileFormWidget(context)
+        : getOtpFormWidget(context),
+      ),
+    );
+
   }
 }
--- lib/views/otp_view.dart
+++ lib/views/otp_view.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:uzachap/shared/colors.dart';
@@ -13,63 +14,33 @@ class OTPView extends StatefulWidget {
   _OTPViewState createState() => _OTPViewState();
 }
 
+
 class _OTPViewState extends State<OTPView> {
+
+  FirebaseAuth _auth  =FirebaseAuth.instance;
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void showSnackbar(String message) {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(message)));
+  }
+  void signInWithPhoneNumber() async {
+    try {
+      final AuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: widget.verId,
+        smsCode: widget.uid,
+      );
+
+      final User user = (await _auth.signInWithCredential(credential)).user;
+
+      showSnackbar("Successfully signed in UID: ${user.uid}");
+    } catch (e) {
+      showSnackbar("Failed to sign in: " + e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/back.png"), fit: BoxFit.cover),
-          ),
-          child: SingleChildScrollView(
-            child: Center(
-              child: Column(
-                children: [
-                  Image.asset("assets/car.png", width: MediaQuery.of(context).size.width,),
-              SizedBox(height: 90,),
-              Column(children: [
-                Text("Enter the code here", style: TextStyle(color: darkText.withOpacity(0.5), fontSize: 18.0, wordSpacing: 3.0, fontWeight: FontWeight.w500),),
-                SizedBox(height: 10.0),
-                Align(
-                  alignment: Alignment.center,
-                    child: Image.asset("assets/arrow.png", height: 12,)),
-                ],
 
-              ),
-                  SizedBox(height: 10.0),
-                  CustomInput(
-                    hintText: '70 000 000 00',
-                    textInputAction: TextInputAction.done,
-                  ),
-                  SizedBox(height: 10.0),
-                  Text("Only correct code will log you in!", style: TextStyle(color: darkText.withOpacity(0.5), fontSize: 16.0, wordSpacing: 2.0, fontWeight: FontWeight.w500),),
-                  SizedBox(height: 20.0),
-                  CustomBtn(
-                    text: "Verify",
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          PageTransition(
-                              child: TownsView(),
-                              type: PageTransitionType.fade));
-                    },
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Text("Uzachap", style: TextStyle(color: darkText.withOpacity(0.5), fontSize: 12.0, wordSpacing: 0.5, fontWeight: FontWeight.w300),)),
-              ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
